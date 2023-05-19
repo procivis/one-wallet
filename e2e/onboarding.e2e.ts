03a2788e988518b2691d863e5dba5a63391b666c
Developer <info@procivis.ch>
2023-05-19 12:52:37 +0200

    ONE-278: added basic page objects

--- e2e/onboarding.e2e.ts
+++ e2e/onboarding.e2e.ts
@@ -1,26 +1,30 @@
 import { expect } from 'detox';
 
+import PinCodeScreen from './page-objects/PinCodeScreen';
+import PinCodeSetScreen from './page-objects/PinCodeSetScreen';
+import WalletScreen from './page-objects/WalletScreen';
+
 describe('Onboarding', () => {
   beforeAll(async () => {
     await device.launchApp({});
   });
 
   it('shows PIN init screen', async () => {
-    await expect(element(by.id('PinCodeInitializationScreen'))).toBeVisible();
+    await expect(PinCodeScreen.Initialization.screen).toBeVisible();
   });
 
   it('shows PIN Set screen after entry & confirmation', async () => {
-    await element(by.id('PinCodeInitializationScreen.keypad.1')).multiTap(6);
-    await element(by.id('PinCodeInitializationScreen.keypad.1')).multiTap(6);
-    await expect(element(by.id('PinCodeSetScreen'))).toBeVisible();
-    await element(by.id('PinCodeSetScreen.close')).tap();
-    await expect(element(by.id('WalletScreen'))).toBeVisible();
+    await PinCodeScreen.Initialization.digit(1).multiTap(6);
+    await PinCodeScreen.Initialization.digit(1).multiTap(6);
+    await expect(PinCodeSetScreen.screen).toBeVisible();
+    await PinCodeSetScreen.closeButton.tap();
+    await expect(WalletScreen.screen).toBeVisible();
   });
 
   it('shows PIN Lock screen after app reopen', async () => {
     await device.reloadReactNative();
-    await expect(element(by.id('PinCodeCheckScreen'))).toBeVisible();
-    await element(by.id('PinCodeCheckScreen.keypad.1')).multiTap(6);
-    await expect(element(by.id('WalletScreen'))).toBeVisible();
+    await expect(PinCodeScreen.Check.screen).toBeVisible();
+    await PinCodeScreen.Check.digit(1).multiTap(6);
+    await expect(WalletScreen.screen).toBeVisible();
   });
 });
