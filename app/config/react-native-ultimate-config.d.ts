9ea75336afbea402b3883ecca415df06e513d762
Developer <info@procivis.ch>
2023-06-21 09:59:41 +0200

    ONE-381: rename demo to test

--- app/config/react-native-ultimate-config.d.ts
+++ app/config/react-native-ultimate-config.d.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 declare module 'react-native-ultimate-config' {
-  interface Config {
-    readonly CONFIG_NAME: 'procivis';
-    readonly DEV_CONFIG: 'true' | 'false';
+  type Flavor = { readonly CONFIG_NAME: 'procivis'; readonly ENVIRONMENT: 'dev' | 'test' | 'demo' };
+
+  type Config = Flavor & {
     readonly APP_NAME: string;
     readonly APP_ID: string;
-  }
+  };
   const config: Readonly<Config>;
   export default config;
 }
