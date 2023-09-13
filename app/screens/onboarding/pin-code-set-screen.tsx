5995b05524d321cad03e5653a2326e975581ee00
Pavel Zářecký <zarecky@procivis.ch>
2023-09-13 14:54:20 +0200

    ONE-792: updated core

--- app/screens/onboarding/pin-code-set-screen.tsx
+++ app/screens/onboarding/pin-code-set-screen.tsx
@@ -6,11 +6,10 @@ import {
 } from '@procivis/react-native-components';
 import { useNavigation } from '@react-navigation/native';
 import React, { FunctionComponent, useCallback, useEffect } from 'react';
-import ONE, { OneError, OneErrorCode } from 'react-native-one-core';
 
+import { useInitializeONECore } from '../../hooks/core-init';
 import { translate } from '../../i18n';
 import { RootNavigationProp } from '../../navigators/root/root-navigator-routes';
-import { reportException } from '../../utils/reporting';
 
 const PinCodeSetScreen: FunctionComponent = () => {
   const rootNavigation = useNavigation<RootNavigationProp<'Onboarding'>>();
@@ -22,21 +21,10 @@ const PinCodeSetScreen: FunctionComponent = () => {
     rootNavigation.replace('Tabs', { screen: 'Wallet' });
   }, [rootNavigation]);
 
-  // create base local identifiers in the wallet
+  const initializeONECore = useInitializeONECore();
   useEffect(() => {
-    const localOrganisationId = '11111111-2222-3333-a444-ffffffffffff';
-    ONE.createOrganisation(localOrganisationId)
-      .catch((err) => {
-        if (err instanceof OneError && err.code === OneErrorCode.AlreadyExists) {
-          return;
-        }
-        throw err;
-      })
-      .then(() => ONE.createLocalDid(`did:key:${Math.floor(Math.random() * 10000000)}`, localOrganisationId))
-      .catch((err) => {
-        reportException(err, 'Failed to create base identifiers');
-      });
-  }, []);
+    initializeONECore();
+  }, [initializeONECore]);
 
   useBlockOSBackNavigation();
 
