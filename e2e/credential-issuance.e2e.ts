ee8b5e927a8b9a87985c8ddbd550d060ed6057b5
Developer <info@procivis.ch>
2023-10-04 14:24:18 +0200

    [ONE-609] e2e tests credential offer

--- e2e/credential-issuance.e2e.ts
+++ e2e/credential-issuance.e2e.ts
@@ -7,24 +7,34 @@ import { bffLogin, createCredential, offerCredential } from './utils/bff-api';
 import { pinSetup } from './utils/init';
 import { scanURL } from './utils/scan';
 
-describe('Credential issuance', () => {
+describe('ONE-601: Credential issuance', () => {
   beforeAll(async () => {
     await device.launchApp({ permissions: { camera: 'YES' } });
     await pinSetup();
   });
 
-  it('completes credential issuance', async () => {
-    const authToken = await bffLogin();
-    const credentialId = await createCredential(authToken);
-    const invitationUrl = await offerCredential(credentialId, authToken);
-    await scanURL(invitationUrl);
+  describe('Offer Screen', () => {
+    beforeEach(async () => {
+      const authToken = await bffLogin();
+      const credentialId = await createCredential(authToken);
+      const invitationUrl = await offerCredential(credentialId, authToken);
+      await scanURL(invitationUrl);
 
-    await expect(CredentialOfferScreen.screen).toBeVisible();
-    await CredentialOfferScreen.acceptButton.tap();
+      await expect(CredentialOfferScreen.screen).toBeVisible();
+    });
 
-    await expect(CredentialAcceptProcessScreen.screen).toBeVisible();
-    await CredentialAcceptProcessScreen.closeButton.tap();
+    it('Accept credential issuance', async () => {
+      await CredentialOfferScreen.acceptButton.tap();
 
-    await expect(WalletScreen.screen).toBeVisible();
+      await expect(CredentialAcceptProcessScreen.screen).toBeVisible();
+      await CredentialAcceptProcessScreen.closeButton.tap();
+
+      await expect(WalletScreen.screen).toBeVisible();
+    });
+
+    it('Reject credential issuance', async () => {
+      await CredentialOfferScreen.rejectButton.tap();
+      await expect(WalletScreen.screen).toBeVisible();
+    });
   });
 });
