110fce577878f84ae766e56e95e528672ec4b0a7
Developer <info@procivis.ch>
2023-11-02 13:48:11 +0100

    ONE-621: E2E revoked credentials

--- e2e/tests/credential-issuance.e2e.ts
+++ e2e/tests/credential-issuance.e2e.ts
@@ -1,22 +1,36 @@
 import { expect } from 'detox';
 
 import CredentialAcceptProcessScreen from '../page-objects/CredentialAcceptProcessScreen';
+import CredentialDetailScreen from '../page-objects/CredentialDetailScreen';
 import CredentialOfferScreen from '../page-objects/CredentialOfferScreen';
+import PinCodeScreen from '../page-objects/PinCodeScreen';
 import WalletScreen from '../page-objects/WalletScreen';
-import { bffLogin, createCredential, offerCredential } from '../utils/bff-api';
+import {
+  bffLogin,
+  createCredential,
+  createCredentialSchema,
+  offerCredential,
+  revokeCredential,
+} from '../utils/bff-api';
 import { pinSetup } from '../utils/init';
 import { scanURL } from '../utils/scan';
 
 describe('ONE-601: Credential issuance', () => {
+  let authToken: string;
+  let credentialSchema: Record<string, any>;
+
   beforeAll(async () => {
     await device.launchApp({ permissions: { camera: 'YES' } });
     await pinSetup();
+    authToken = await bffLogin();
+    credentialSchema = await createCredentialSchema(authToken);
   });
 
-  describe('Offer Screen', () => {
+  describe('Credential offer', () => {
+    let credentialId: string;
+
     beforeEach(async () => {
-      const authToken = await bffLogin();
-      const credentialId = await createCredential(authToken);
+      credentialId = await createCredential(authToken, credentialSchema);
       const invitationUrl = await offerCredential(credentialId, authToken);
       await scanURL(invitationUrl);
 
@@ -30,11 +44,52 @@ describe('ONE-601: Credential issuance', () => {
       await CredentialAcceptProcessScreen.closeButton.tap();
 
       await expect(WalletScreen.screen).toBeVisible();
+      await expect(WalletScreen.credential(credentialId).element).toBeVisible();
     });
 
     it('Reject credential issuance', async () => {
       await CredentialOfferScreen.rejectButton.tap();
       await expect(WalletScreen.screen).toBeVisible();
+    });
+  });
+
+  describe('ONE-620: Credential revocation', () => {
+    let credentialId: string;
+
+    beforeAll(async () => {
+      credentialId = await createCredential(authToken, credentialSchema);
+      const invitationUrl = await offerCredential(credentialId, authToken);
+      await scanURL(invitationUrl);
+
+      await expect(CredentialOfferScreen.screen).toBeVisible();
+      await CredentialOfferScreen.acceptButton.tap();
+      await expect(CredentialAcceptProcessScreen.screen).toBeVisible();
+      await CredentialAcceptProcessScreen.closeButton.tap();
+      await expect(WalletScreen.screen).toBeVisible();
+    });
+
+    it('Credential not revoked initially', async () => {
+      await expect(WalletScreen.credential(credentialId).element).toBeVisible();
+      await expect(WalletScreen.credential(credentialId).revokedLabel).not.toExist();
+    });
+
+    it('Credential revoked remotely', async () => {
+      await revokeCredential(credentialId, authToken);
+      await device.reloadReactNative();
+      await expect(PinCodeScreen.Check.screen).toBeVisible();
+      await PinCodeScreen.Check.digit(1).multiTap(6);
+      await expect(WalletScreen.screen).toBeVisible();
+
+      await expect(WalletScreen.credential(credentialId).element).toBeVisible();
+      await expect(WalletScreen.credential(credentialId).revokedLabel).toExist();
+    });
+
+    it('Revoked credential detail screen', async () => {
+      await WalletScreen.credential(credentialId).element.tap();
+      await expect(CredentialDetailScreen.screen).toBeVisible();
+      await expect(CredentialDetailScreen.status.element).toBeVisible();
+      await expect(CredentialDetailScreen.status.value).toHaveText('Revoked');
+      await expect(CredentialDetailScreen.log('revoked')).toExist();
     });
   });
 });
--- e2e/tests/proof-request.e2e.ts
+++ e2e/tests/proof-request.e2e.ts
@@ -4,19 +4,38 @@ import CredentialAcceptProcessScreen from '../page-objects/CredentialAcceptProce
 import CredentialOfferScreen from '../page-objects/CredentialOfferScreen';
 import ProofRequestAcceptProcessScreen from '../page-objects/ProofRequestAcceptProcessScreen';
 import ProofRequestSharingScreen from '../page-objects/ProofRequestScreen';
+import ProofRequestSelectCredentialScreen from '../page-objects/ProofRequestSelectCredentialScreen';
 import WalletScreen from '../page-objects/WalletScreen';
-import { bffLogin, createCredential, createProofRequest, offerCredential, requestProof } from '../utils/bff-api';
+import {
+  bffLogin,
+  createCredential,
+  createCredentialSchema,
+  createProofRequest,
+  createProofSchema,
+  offerCredential,
+  requestProof,
+  revokeCredential,
+} from '../utils/bff-api';
 import { verifyButtonEnabled } from '../utils/button';
 import { pinSetup } from '../utils/init';
 import { scanURL } from '../utils/scan';
 
 describe('ONE-614: Proof request', () => {
-  describe('Proof request with Credentials', () => {
+  let authToken: string;
+  let credentialSchema: Record<string, any>;
+  let proofSchema: Record<string, any>;
+
+  beforeAll(async () => {
+    authToken = await bffLogin();
+    credentialSchema = await createCredentialSchema(authToken);
+    proofSchema = await createProofSchema(authToken, credentialSchema);
+  });
+
+  describe('Proof request with valid credential', () => {
     beforeAll(async () => {
       await device.launchApp({ permissions: { camera: 'YES' }, delete: true });
       await pinSetup();
-      const authToken = await bffLogin();
-      const credentialId = await createCredential(authToken);
+      const credentialId = await createCredential(authToken, credentialSchema);
       const invitationUrl = await offerCredential(credentialId, authToken);
       await scanURL(invitationUrl);
       await expect(CredentialOfferScreen.screen).toBeVisible();
@@ -27,8 +46,7 @@ describe('ONE-614: Proof request', () => {
     });
 
     beforeEach(async () => {
-      const authToken = await bffLogin();
-      const proofRequestId = await createProofRequest(authToken);
+      const proofRequestId = await createProofRequest(authToken, proofSchema);
       const invitationUrl = await requestProof(proofRequestId, authToken);
       await scanURL(invitationUrl);
 
@@ -55,12 +73,90 @@ describe('ONE-614: Proof request', () => {
     });
 
     it('Without credentials', async () => {
-      const authToken = await bffLogin();
       const proofRequestId = await createProofRequest(authToken);
       const invitationUrl = await requestProof(proofRequestId, authToken);
       await scanURL(invitationUrl);
       await expect(ProofRequestSharingScreen.screen).toBeVisible();
       await verifyButtonEnabled(ProofRequestSharingScreen.shareButton, false);
+    });
+  });
+
+  describe('ONE-620: Revoked credentials', () => {
+    const credentialIds: string[] = [];
+    beforeAll(async () => {
+      await device.launchApp({ permissions: { camera: 'YES' }, delete: true });
+      await pinSetup();
+
+      for (let i = 0; i < 3; i++) {
+        const credentialId = await createCredential(authToken, credentialSchema);
+        const invitationUrl = await offerCredential(credentialId, authToken);
+        await scanURL(invitationUrl);
+        await expect(CredentialOfferScreen.screen).toBeVisible();
+        await CredentialOfferScreen.acceptButton.tap();
+        await expect(CredentialAcceptProcessScreen.screen).toBeVisible();
+        await CredentialAcceptProcessScreen.closeButton.tap();
+        credentialIds.push(credentialId);
+      }
+    });
+
+    it('2 valid one revoked', async () => {
+      await revokeCredential(credentialIds[2], authToken);
+      const proofRequestId = await createProofRequest(authToken, proofSchema);
+      const invitationUrl = await requestProof(proofRequestId, authToken);
+      await scanURL(invitationUrl);
+
+      await expect(ProofRequestSharingScreen.screen).toBeVisible();
+      await verifyButtonEnabled(ProofRequestSharingScreen.shareButton, true);
+
+      await expect(ProofRequestSharingScreen.credential(0).element).toBeVisible();
+      await expect(ProofRequestSharingScreen.credential(0).title(credentialIds[1])).toExist();
+      await expect(ProofRequestSharingScreen.credential(0).notice.multiple.element).toBeVisible();
+      await ProofRequestSharingScreen.credential(0).notice.multiple.selectButton.tap();
+
+      await expect(ProofRequestSelectCredentialScreen.screen).toBeVisible();
+      await expect(ProofRequestSelectCredentialScreen.credential(credentialIds[0]).element).toExist();
+      await expect(ProofRequestSelectCredentialScreen.credential(credentialIds[0]).unselected).toExist();
+      // latest valid item is selected
+      await expect(ProofRequestSelectCredentialScreen.credential(credentialIds[1]).element).toExist();
+      await expect(ProofRequestSelectCredentialScreen.credential(credentialIds[1]).selected).toExist();
+
+      await expect(ProofRequestSelectCredentialScreen.credential(credentialIds[2]).element).not.toExist();
+      await ProofRequestSelectCredentialScreen.backButton.tap();
+      await ProofRequestSharingScreen.cancelButton.tap();
+      await expect(WalletScreen.screen).toBeVisible();
+    });
+
+    it('2 revoked one valid', async () => {
+      await revokeCredential(credentialIds[1], authToken);
+      const proofRequestId = await createProofRequest(authToken, proofSchema);
+      const invitationUrl = await requestProof(proofRequestId, authToken);
+      await scanURL(invitationUrl);
+
+      await expect(ProofRequestSharingScreen.screen).toBeVisible();
+      await verifyButtonEnabled(ProofRequestSharingScreen.shareButton, true);
+
+      await expect(ProofRequestSharingScreen.credential(0).element).toBeVisible();
+      // no selection possible
+      await expect(ProofRequestSharingScreen.credential(0).notice.multiple.element).not.toExist();
+      await expect(ProofRequestSharingScreen.credential(0).title(credentialIds[0])).toExist();
+      await ProofRequestSharingScreen.cancelButton.tap();
+      await expect(WalletScreen.screen).toBeVisible();
+    });
+
+    it('all revoked', async () => {
+      await revokeCredential(credentialIds[0], authToken);
+      const proofRequestId = await createProofRequest(authToken, proofSchema);
+      const invitationUrl = await requestProof(proofRequestId, authToken);
+      await scanURL(invitationUrl);
+
+      await expect(ProofRequestSharingScreen.screen).toBeVisible();
+      await verifyButtonEnabled(ProofRequestSharingScreen.shareButton, false);
+
+      await expect(ProofRequestSharingScreen.credential(0).element).toBeVisible();
+      await expect(ProofRequestSharingScreen.credential(0).subtitle.revoked).toExist();
+      await expect(ProofRequestSharingScreen.credential(0).notice.revoked).toExist();
+      await ProofRequestSharingScreen.cancelButton.tap();
+      await expect(WalletScreen.screen).toBeVisible();
     });
   });
 });
