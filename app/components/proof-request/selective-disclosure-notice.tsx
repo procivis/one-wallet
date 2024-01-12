1cd3fe3cc0476f313c446d7fd010c84ef8e321e2
Developer <info@procivis.ch>
2024-01-12 11:53:23 +0100

    ONE-1150: selective disclosure

---
+++ app/components/proof-request/selective-disclosure-notice.tsx
@@ -0,0 +1,40 @@
+import {
+  Typography,
+  useAppColorScheme,
+} from '@procivis/react-native-components';
+import React, { FunctionComponent } from 'react';
+import { StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
+
+import { translate } from '../../i18n';
+import { AlertOutlineIcon } from '../icon/credential-icon';
+
+export const SelectiveDislosureNotice: FunctionComponent<{
+  style?: StyleProp<ViewStyle>;
+  testID?: string;
+}> = ({ testID, style }) => {
+  const colorScheme = useAppColorScheme();
+  return (
+    <View
+      style={[styles.notice, { backgroundColor: colorScheme.notice }, style]}
+      testID={testID}
+    >
+      <AlertOutlineIcon />
+      <Typography color={colorScheme.noticeText} size="sml" style={styles.text}>
+        {translate('proofRequest.selectiveDisclosure.notice')}
+      </Typography>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  notice: {
+    borderRadius: 4,
+    flexDirection: 'row',
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+  },
+  text: {
+    flex: 1,
+    marginLeft: 8,
+  },
+});
