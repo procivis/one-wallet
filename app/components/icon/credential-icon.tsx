a69bb61adc8563611ea4a70f75f6ce7b1dcec3b4
Pavel Zarecky <zarecky@procivis.ch>
2024-01-12 11:53:23 +0100

    ONE-1150: selective disclosure

--- app/components/icon/credential-icon.tsx
+++ app/components/icon/credential-icon.tsx
@@ -22,3 +22,21 @@ export const MissingCredentialIcon: React.FunctionComponent<SvgProps> = ({
     </Svg>
   );
 };
+
+// https://www.figma.com/file/S3WwgTMHuqxAsfu5zElCzq/App-Icon-Library-(Design)?type=design&node-id=404-355&mode=design&t=DIJGwVXRoMIeamtV-4
+export const AlertOutlineIcon: React.FunctionComponent<SvgProps> = ({
+  ...props
+}) => {
+  const colorScheme = useAppColorScheme();
+  return (
+    <Svg fill="none" height={24} viewBox="0 0 24 24" width={24} {...props}>
+      <Path
+        d="M10.6198 5.76789C11.4007 4.49881 13.2454 4.49882 14.0264 5.76789L21.7703 18.3518C22.5904 19.6843 21.6317 21.4 20.067 21.4H4.57912C3.01448 21.4 2.05579 19.6843 2.87581 18.3518L10.6198 5.76789Z"
+        fillRule="evenodd"
+        stroke={colorScheme.text}
+      />
+      <Rect fill={colorScheme.text} height="1" width="1" x="12" y="18" />
+      <Rect fill={colorScheme.text} height="5.75" width="1" x="12" y="10" />
+    </Svg>
+  );
+};
--- app/utils/credential.ts
+++ app/utils/credential.ts
@@ -1,16 +1,34 @@
 import { formatDate } from '@procivis/react-native-components';
-import { Claim } from 'react-native-one-core';
+import {
+  Claim,
+  Config,
+  CredentialListItem,
+  FormatFeatureEnum,
+} from 'react-native-one-core';
 
 /**
  * Format claim value for UI presentation
  */
-export const formatClaimValue = (claim: Claim) => {
-  switch (claim.dataType) {
-    case 'DATE':
-    case 'BIRTH_DATE': {
+export const formatClaimValue = (claim: Claim, config: Config) => {
+  switch (config.datatype[claim.dataType]?.type) {
+    case 'DATE': {
       return formatDate(new Date(claim.value)) ?? claim.value;
     }
     default:
       return claim.value;
   }
+};
+
+export const supportsSelectiveDisclosure = (
+  credential: CredentialListItem | undefined,
+  config: Config | undefined,
+) => {
+  const formatConfig = credential && config?.format[credential.schema.format];
+  return formatConfig
+    ? Boolean(
+        formatConfig.capabilities?.features?.includes(
+          FormatFeatureEnum.SelectiveDisclosure,
+        ),
+      )
+    : undefined;
 };
