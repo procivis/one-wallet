9d37fdc390015d56110eba96640c3be13bd59d22
Developer <info@procivis.ch>
2024-01-12 22:21:24 +0100

    * domain updates

--- e2e/utils/button.ts
+++ e2e/utils/button.ts
@@ -3,10 +3,17 @@ import { expect } from 'detox';
 /**
  * Check whether button element is disabled/enabled
  */
-export async function verifyButtonEnabled(buttonElement: Detox.NativeElement, enabled: boolean) {
+export async function verifyButtonEnabled(
+  buttonElement: Detox.NativeElement,
+  enabled: boolean,
+) {
   const buttonAttributes = await buttonElement.getAttributes();
   if ('identifier' in buttonAttributes) {
-    const buttonStateMarker = element(by.id(`${buttonAttributes.identifier}.${enabled ? 'enabled' : 'disabled'}`));
+    const buttonStateMarker = element(
+      by.id(
+        `${buttonAttributes.identifier}.${enabled ? 'enabled' : 'disabled'}`,
+      ),
+    );
     await expect(buttonStateMarker).toBeVisible();
   } else {
     throw Error('Invalid Button element');
