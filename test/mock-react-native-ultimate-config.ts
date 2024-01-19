fd8540ecd64d43a936b1a6f6ccd23559e587c743
Pavel Zářecký <zarecky@procivis.ch>
2024-01-19 09:27:32 +0100

    ONE-1279: updated app branding

--- test/mock-react-native-ultimate-config.ts
+++ test/mock-react-native-ultimate-config.ts
@@ -1,6 +1,6 @@
 jest.mock('react-native-ultimate-config', () => ({
   APP_ID: 'ch.procivis.one.wallet.dev',
-  APP_NAME: 'ONE+',
+  APP_NAME: 'Procivis One Wallet',
   CONFIG_NAME: 'procivis',
   ENVIRONMENT: 'test',
 }));
