6c497c141ebe4297404fff11176d45e7bfe1a1ea
Developer <info@procivis.ch>
2024-01-26 10:33:34 +0100

    [ONE-899] Turn off biometrics missing success screen

--- app/screens/onboarding/pin-code-set-screen.tsx
+++ app/screens/onboarding/pin-code-set-screen.tsx
@@ -1,8 +1,12 @@
-import { LoadingResultState } from '@procivis/react-native-components';
+import {
+  LoadingResult,
+  LoadingResultState,
+  LoadingResultVariation,
+  useBlockOSBackNavigation,
+} from '@procivis/react-native-components';
 import { useNavigation } from '@react-navigation/native';
 import React, { FC, useCallback, useEffect, useState } from 'react';
 
-import PinCodeSet from '../../components/pin-code/pin-code-set';
 import { useInitializeONECoreIdentifiers } from '../../hooks/core-init';
 import { translate } from '../../i18n';
 import { RootNavigationProp } from '../../navigators/root/root-navigator-routes';
@@ -12,12 +16,6 @@ const PinCodeSetScreen: FC = () => {
   const [state, setState] = useState(LoadingResultState.InProgress);
   const initializeONECoreIdentifiers = useInitializeONECoreIdentifiers();
 
-  useEffect(() => {
-    initializeONECoreIdentifiers()
-      .then(() => setState(LoadingResultState.Success))
-      .catch(() => setState(LoadingResultState.Failure));
-  }, [initializeONECoreIdentifiers]);
-
   const onClose = useCallback(() => {
     if (rootNavigation.canGoBack()) {
       rootNavigation.popToTop();
@@ -25,12 +23,25 @@ const PinCodeSetScreen: FC = () => {
     rootNavigation.replace('Tabs', { screen: 'Wallet' });
   }, [rootNavigation]);
 
+  useEffect(() => {
+    initializeONECoreIdentifiers()
+      .then(() => setState(LoadingResultState.Success))
+      .catch(() => setState(LoadingResultState.Failure));
+  }, [initializeONECoreIdentifiers]);
+
+  useBlockOSBackNavigation();
+
   return (
-    <PinCodeSet
+    <LoadingResult
+      failureCloseButtonLabel={translate('common.close')}
+      inProgressCloseButtonLabel={translate('common.close')}
       onClose={onClose}
       state={state}
       subtitle={translate('onboarding.pinCodeSet.description')}
+      successCloseButtonLabel={translate('common.continue')}
+      testID="PinCodeSetScreen"
       title={translate('onboarding.pinCodeSet.title')}
+      variation={LoadingResultVariation.Neutral}
     />
   );
 };
---
+++ app/screens/settings/biometrics-set-screen.tsx
@@ -0,0 +1,46 @@
+import {
+  LoadingResult,
+  LoadingResultState,
+  LoadingResultVariation,
+  useBlockOSBackNavigation,
+} from '@procivis/react-native-components';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import React, { FC, useCallback } from 'react';
+
+import { translate } from '../../i18n';
+import {
+  SettingsNavigationProp,
+  SettingsRouteProp,
+} from '../../navigators/settings/settings-routes';
+
+const BiometricsSetScreen: FC = () => {
+  const navigation = useNavigation<SettingsNavigationProp<'BiometricsSet'>>();
+  const route = useRoute<SettingsRouteProp<'BiometricsSet'>>();
+  const biometricsStateKey = route.params.enabled ? 'enabled' : 'disabled';
+
+  const onClose = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
+  useBlockOSBackNavigation();
+
+  return (
+    <LoadingResult
+      failureCloseButtonLabel={translate('common.close')}
+      inProgressCloseButtonLabel={translate('common.close')}
+      onClose={onClose}
+      state={LoadingResultState.Success}
+      subtitle={translate(
+        `wallet.settings.security.biometricsSet.${biometricsStateKey}.description`,
+      )}
+      successCloseButtonLabel={translate('common.continue')}
+      testID="BiometricsSetScreen"
+      title={translate(
+        `wallet.settings.security.biometricsSet.${biometricsStateKey}.title`,
+      )}
+      variation={LoadingResultVariation.Neutral}
+    />
+  );
+};
+
+export default BiometricsSetScreen;
