fbb7b3589f70c56f3a8b4de7ed9709973fcea6ff
Developer <info@procivis.ch>
2024-02-05 15:22:29 +0100

    [ONE-1534] Add search to history screen

--- app/hooks/history.ts
+++ app/hooks/history.ts
@@ -1,27 +1,33 @@
+import { HistoryListQuery } from '@procivis/react-native-one-core';
 import { useQuery } from 'react-query';
 
-import { groupEntriesByMonth } from '../utils/history';
+import {
+  getQueryKeyFromListQueryParams,
+  groupEntriesByMonth,
+} from '../utils/history';
 import { useONECore } from './core-context';
 import { ONE_CORE_ORGANISATION_ID } from './core-init';
 
 const HISTORY_LIST_QUERY_KEY = 'history-list';
 
-export const useHistory = () => {
+export const useHistory = (extraQueryParams?: Partial<HistoryListQuery>) => {
   const { core } = useONECore();
 
+  const queryParams = {
+    organisationId: ONE_CORE_ORGANISATION_ID,
+    page: 0,
+    // TODO: workaround pagination for now, until it's supported by UI
+    pageSize: 10000,
+    ...extraQueryParams,
+  };
+
   return useQuery(
-    [HISTORY_LIST_QUERY_KEY],
-    () =>
-      core
-        .getHistory({
-          organisationId: ONE_CORE_ORGANISATION_ID,
-          page: 0,
-          // TODO: workaround pagination for now, until it's supported by UI
-          pageSize: 10000,
-        })
-        .then(({ values }) => groupEntriesByMonth(values)),
-    {
-      keepPreviousData: true,
+    [HISTORY_LIST_QUERY_KEY, ...getQueryKeyFromListQueryParams(queryParams)],
+    async () => {
+      return core
+        .getHistory(queryParams)
+        .then(({ values }) => groupEntriesByMonth(values));
     },
+    { keepPreviousData: true },
   );
 };
