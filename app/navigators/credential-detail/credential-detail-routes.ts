ad2eba37af912fddc64c91a18a7718ccce57cb1a
Developer <info@procivis.ch>
2024-02-23 13:52:31 +0100

    [ONE-1645] Add credential validity check to details screen

---
+++ app/navigators/credential-detail/credential-detail-routes.ts
@@ -0,0 +1,21 @@
+import { RouteProp } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+export type CredentialDetailNavigatorParamList = {
+  DeleteProcessing: {
+    credentialId: string;
+  };
+  Detail: {
+    credentialId: string;
+  };
+  ValidityProcessing: {
+    credentialId: string;
+  };
+};
+
+export type CredentialDetailRouteProp<
+  RouteName extends keyof CredentialDetailNavigatorParamList,
+> = RouteProp<CredentialDetailNavigatorParamList, RouteName>;
+export type CredentialDetailNavigationProp<
+  RouteName extends keyof CredentialDetailNavigatorParamList,
+> = NativeStackNavigationProp<CredentialDetailNavigatorParamList, RouteName>;
