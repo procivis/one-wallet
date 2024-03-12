a42bab5502da1a0a4358851cba6c4baea67ab90b
Developer <info@procivis.ch>
2024-03-12 16:10:29 +0100

    ONE-1711: initialize with HW and SW keys

--- app/models/wallet-store/wallet-store.test.ts
+++ app/models/wallet-store/wallet-store.test.ts
@@ -2,7 +2,8 @@ import { WalletStoreModel } from './wallet-store';
 
 test('can be created', () => {
   const instance = WalletStoreModel.create({
-    holderDidId: '',
+    holderDidHwId: '',
+    holderDidSwId: '',
   });
 
   expect(instance).toBeTruthy();
