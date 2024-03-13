f9764509bc6b697eeb7a35e3ade9818ff3fdc7b5
Pavel Zarecky <zarecky@procivis.ch>
2024-03-13 16:55:20 +0100

    ONE-1707: select did based on issuer requirements

--- app/screens/credential/credential-accept-process-screen.tsx
+++ app/screens/credential/credential-accept-process-screen.tsx
@@ -4,11 +4,13 @@ import {
   LoadingResultVariation,
   useBlockOSBackNavigation,
 } from '@procivis/react-native-components';
+import { WalletStorageType } from '@procivis/react-native-one-core';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import React, {
   FunctionComponent,
   useCallback,
   useEffect,
+  useMemo,
   useState,
 } from 'react';
 import { Linking } from 'react-native';
@@ -17,7 +19,7 @@ import {
   useCredentialAccept,
   useCredentialDetail,
 } from '../../hooks/credentials';
-import { translate } from '../../i18n';
+import { translate, TxKeyPath } from '../../i18n';
 import { useStores } from '../../models';
 import { IssueCredentialRouteProp } from '../../navigators/issue-credential/issue-credential-routes';
 import { RootNavigationProp } from '../../navigators/root/root-routes';
@@ -33,48 +35,71 @@ const CredentialAcceptProcessScreen: FunctionComponent = () => {
   >(LoadingResultState.InProgress);
   const { credentialId, interactionId } = route.params;
   const { mutateAsync: acceptCredential } = useCredentialAccept();
-  const { data: credential, refetch: refetchCredential } =
-    useCredentialDetail(credentialId);
+  const { data: credential } = useCredentialDetail(credentialId);
   const { walletStore } = useStores();
 
   useBlockOSBackNavigation();
 
+  const requiredStorageType = credential?.schema.walletStorageType;
+  const didId = useMemo(() => {
+    switch (requiredStorageType) {
+      case WalletStorageType.SOFTWARE:
+        return walletStore.holderDidSwId;
+      case WalletStorageType.HARDWARE:
+        return walletStore.holderDidHwId;
+      default:
+        return walletStore.holderDidId;
+    }
+  }, [walletStore, requiredStorageType]);
+
   const handleCredentialAccept = useCallback(async () => {
+    if (!didId) {
+      setState(LoadingResultState.Failure);
+      return;
+    }
+
     try {
-      await acceptCredential({ didId: walletStore.holderDidId, interactionId });
-      await refetchCredential();
+      await acceptCredential({ didId, interactionId });
       setState(LoadingResultState.Success);
     } catch (e) {
       reportException(e, 'Accept credential failure');
       setState(LoadingResultState.Failure);
     }
-  }, [acceptCredential, interactionId, refetchCredential, walletStore]);
+  }, [acceptCredential, interactionId, didId]);
 
   useEffect(() => {
-    handleCredentialAccept();
+    if (credential) {
+      handleCredentialAccept();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [!credential]);
 
+  const redirectUri = credential?.redirectUri;
   const onCTA = useCallback(() => {
-    credential?.redirectUri &&
-      Linking.openURL(credential.redirectUri).catch((e) => {
+    redirectUri &&
+      Linking.openURL(redirectUri).catch((e) => {
         reportException(e, "Couldn't open redirect URI");
       });
-  }, [credential]);
+  }, [redirectUri]);
 
   const onClose = useCallback(() => {
     rootNavigation.navigate('Tabs', { screen: 'Wallet' });
   }, [rootNavigation]);
+
+  const subtitle: TxKeyPath =
+    state === LoadingResultState.Failure && !didId
+      ? 'credentialAccept.failure.incompatible'
+      : `credentialAccept.${state}.subtitle`;
 
   return (
     <LoadingResult
       ctaButtonLabel={translate('credentialAccept.success.cta')}
       failureCloseButtonLabel={translate('common.close')}
       inProgressCloseButtonLabel={translate('common.cancel')}
-      onCTA={credential?.redirectUri ? onCTA : undefined}
+      onCTA={redirectUri ? onCTA : undefined}
       onClose={onClose}
       state={state}
-      subtitle={translate(`credentialAccept.${state}.subtitle`)}
+      subtitle={translate(subtitle)}
       successCloseButtonLabel={translate('common.close')}
       testID="CredentialAcceptProcessScreen"
       title={translate(`credentialAccept.${state}.title`)}
