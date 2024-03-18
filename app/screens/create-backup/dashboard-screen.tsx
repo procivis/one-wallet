cb91fb5447bca97510f45f226f5891fe81bad30e
Developer <info@procivis.ch>
2024-03-18 14:15:58 +0100

    [ONE-1835] Show backup contents in history details

--- app/screens/create-backup/dashboard-screen.tsx
+++ app/screens/create-backup/dashboard-screen.tsx
@@ -1,17 +1,54 @@
-import { FlatListView } from '@procivis/react-native-components';
+import {
+  FlatListView,
+  useAppColorScheme,
+} from '@procivis/react-native-components';
+import { HistoryEntityTypeEnum } from '@procivis/react-native-one-core';
 import { useNavigation } from '@react-navigation/native';
-import React, { FC } from 'react';
+import React, { FC, useCallback, useMemo } from 'react';
 import { StyleSheet } from 'react-native';
 
-import BackupScreen from '../../components/backup/backup-screen';
+import { BackupScreen } from '../../components/backup/backup-screen';
 import { Section } from '../../components/common/section';
+import { NextIcon } from '../../components/icon/common-icon';
+import { useHistory } from '../../hooks/history';
 import { translate } from '../../i18n';
 import { CreateBackupNavigationProp } from '../../navigators/create-backup/create-backup-routes';
+import { SettingsNavigationProp } from '../../navigators/settings/settings-routes';
+import { formatTimestamp } from '../../utils/date';
+import { getEntryTitle } from '../../utils/history';
 
 const DashboardScreen: FC = () => {
+  const colorScheme = useAppColorScheme();
   const navigation = useNavigation<CreateBackupNavigationProp<'Dashboard'>>();
-  // TODO: replace with real data
-  const lastBackupItems: any[] = [];
+  const settingsNavigation = useNavigation<SettingsNavigationProp>();
+  const { data: historyData } = useHistory({
+    entityTypes: [HistoryEntityTypeEnum.BACKUP],
+    pageSize: 1,
+  });
+  const lastBackupEntry = historyData?.pages?.[0]?.values?.[0];
+
+  const items = useMemo(() => {
+    if (!lastBackupEntry) {
+      return [];
+    }
+    return [
+      {
+        rightAccessory: <NextIcon color={colorScheme.text} />,
+        subtitle: formatTimestamp(new Date(lastBackupEntry.createdDate)),
+        testID: 'CreateBackupDashboardScreen.history',
+        title: getEntryTitle(lastBackupEntry),
+      },
+    ];
+  }, [colorScheme.text, lastBackupEntry]);
+
+  const handleItemPress = useCallback(() => {
+    settingsNavigation.navigate('History', {
+      params: {
+        entry: lastBackupEntry!,
+      },
+      screen: 'Detail',
+    });
+  }, [lastBackupEntry, settingsNavigation]);
 
   return (
     <BackupScreen
@@ -29,7 +66,8 @@ const DashboardScreen: FC = () => {
         <FlatListView
           emptyListSubtitle={translate('createBackup.dashboard.empty.subtitle')}
           emptyListTitle={translate('createBackup.dashboard.empty.title')}
-          items={lastBackupItems}
+          items={items}
+          onItemSelected={handleItemPress}
           style={styles.noHorizontalPadding}
         />
       </Section>
