d2f65dc46ac1c0034bdf568f1dc9fdfcd54e6a78
Developer <info@procivis.ch>
2024-04-02 14:39:57 +0200

    ONE-1973: disable dark mode

--- app/theme/flavors/procivis/color.ts
+++ app/theme/flavors/procivis/color.ts
@@ -1,22 +1,5 @@
 import { ColorScheme } from '@procivis/one-react-native-components';
 
-export const darkModeColorScheme: ColorScheme = {
-  accent: '#0F151A',
-  accentText: '#EBF2F4',
-  background: '#F1F3F5',
-  backgroundScroll: 'rgba(241, 243, 245, 0.7)',
-  black: '#000000',
-  codeAccent: '#CFD2E7',
-  credentialHeaderBackground: 'rgba(255, 255, 255, 0.87)',
-  error: '#D90D0D',
-  grayDark: '#CDD4DA',
-  linkText: '#3F7BA6',
-  success: '#00D066',
-  text: '#0D0E10',
-  warning: '#F7BF0B',
-  white: '#FFFFFF',
-};
-
 export const colorScheme: ColorScheme = {
   accent: '#0F151A',
   accentText: '#EBF2F4',
@@ -25,6 +8,7 @@ export const colorScheme: ColorScheme = {
   black: '#000000',
   codeAccent: '#CFD2E7',
   credentialHeaderBackground: 'rgba(255, 255, 255, 0.87)',
+  darkMode: false,
   error: '#D90D0D',
   grayDark: '#CDD4DA',
   linkText: '#3F7BA6',
