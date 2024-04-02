aa2faa7ed3aa48e02c0910475253bc5071366880
Developer <info@procivis.ch>
2024-04-02 13:22:56 +0200

    [ONE-1657] e2e lvvc

--- e2e/utils/init.ts
+++ e2e/utils/init.ts
@@ -1,6 +1,9 @@
+import { expect } from 'detox';
+
 import OnboardingSetupScreen from '../page-objects/OnboardingSetupScreen';
 import PinCodeScreen from '../page-objects/PinCodeScreen';
 import PinCodeSetScreen from '../page-objects/PinCodeSetScreen';
+import WalletScreen from '../page-objects/WalletScreen';
 
 /**
  * correct app PIN is '111111' used among all test suites
@@ -12,4 +15,23 @@ export async function pinSetup() {
   await PinCodeScreen.Initialization.digit(CORRECT_PIN_DIGIT).multiTap(6);
   await PinCodeScreen.Initialization.digit(CORRECT_PIN_DIGIT).multiTap(6);
   await PinCodeSetScreen.closeButton.tap();
+}
+
+export async function reloadApp() {
+  await device.launchApp({ newInstance: true });
+  await expect(PinCodeScreen.Check.screen).toBeVisible();
+  await PinCodeScreen.Check.digit(CORRECT_PIN_DIGIT).multiTap(6);
+  await expect(WalletScreen.screen).toBeVisible();
+}
+
+type LaunchAppConfig = {
+  delete?: boolean;
+};
+
+export async function launchApp(config?: LaunchAppConfig) {
+  await device.launchApp({
+    delete: config?.delete,
+    permissions: { camera: 'YES' },
+  });
+  await pinSetup();
 }
