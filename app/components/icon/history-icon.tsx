282e9d5d98c97807823e312c1323fff87ca00e1e
Eugeniu Rusu <eg@Eugenius-MacBook-Pro.local>
2024-04-15 13:22:21 +0300

    Updated history icon figma links

--- app/components/icon/history-icon.tsx
+++ app/components/icon/history-icon.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import Svg, { Path, Rect, SvgProps } from 'react-native-svg';
 
-// https://www.figma.com/file/52qDYWUMjXAGre1dcnz5bz/Procivis-One-Wallet?type=design&node-id=1143-40313&mode=design&t=zjIHgYZjubQkuXyJ-0
+// https://www.figma.com/file/52qDYWUMjXAGre1dcnz5bz/Procivis-One-Wallet?type=design&node-id=454-99609&mode=design&t=mnpIjCXoZSU3mT8I-0
 export const IssuedSuccessIcon: FC<SvgProps> = () => (
   <Svg fill="none" height="48" viewBox="0 0 48 48" width="48">
     <Rect fill="#F1F3F5" height="48" rx="24" width="48" />
@@ -26,7 +26,7 @@ export const IssuedSuccessIcon: FC<SvgProps> = () => (
   </Svg>
 );
 
-// https://www.figma.com/file/52qDYWUMjXAGre1dcnz5bz/Procivis-One-Wallet?type=design&node-id=1143-40313&mode=design&t=zjIHgYZjubQkuXyJ-0
+// https://www.figma.com/file/52qDYWUMjXAGre1dcnz5bz/Procivis-One-Wallet?type=design&node-id=1143-41498&mode=design&t=mnpIjCXoZSU3mT8I-4
 export const ProofRejectedIcon: FC<SvgProps> = () => (
   <Svg fill="none" height="48" viewBox="0 0 48 48" width="48">
     <Rect fill="#F1F3F5" height="48" rx="24" width="48" />
@@ -51,7 +51,7 @@ export const ProofRejectedIcon: FC<SvgProps> = () => (
   </Svg>
 );
 
-// https://www.figma.com/file/52qDYWUMjXAGre1dcnz5bz/Procivis-One-Wallet?type=design&node-id=1143-40313&mode=design&t=zjIHgYZjubQkuXyJ-0
+// https://www.figma.com/file/52qDYWUMjXAGre1dcnz5bz/Procivis-One-Wallet?type=design&node-id=454-100366&mode=design&t=mnpIjCXoZSU3mT8I-4
 export const ProofSuccessIcon: FC<SvgProps> = () => (
   <Svg fill="none" height="48" viewBox="0 0 48 48" width="48">
     <Rect fill="#F1F3F5" height="48" rx="24" width="48" />
