72574fa736021aa32ea8b0799ade588d0f692b3c
Developer <info@procivis.ch>
2024-04-19 11:51:36 +0200

    Added invitation error details screen

--- app/navigators/invitation/invitation-navigator.tsx
+++ app/navigators/invitation/invitation-navigator.tsx
@@ -1,6 +1,8 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import { Platform } from 'react-native';
 
+import InvitationErrorDetailsScreen from '../../screens/credential/invitation-error-details-screen';
 import InvitationProcessScreen from '../../screens/credential/invitation-process-screen';
 import { InvitationNavigatorParamList } from './invitation-routes';
 
@@ -10,6 +12,16 @@ const InvitationNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen component={InvitationProcessScreen} name="Processing" />
+      <Stack.Screen
+        component={InvitationErrorDetailsScreen}
+        name="Error"
+        options={{
+          animation:
+            Platform.OS === 'android' ? 'slide_from_bottom' : undefined,
+          headerShown: false,
+          presentation: 'fullScreenModal',
+        }}
+      />
     </Stack.Navigator>
   );
 };
