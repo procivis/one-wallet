6111b28b4419f689af16003f67ccd29e92731bd1
Developer <info@procivis.ch>
2024-04-22 18:36:01 +0200

    Fixed navigation for restore backup flow

--- app/navigators/create-backup/create-backup-routes.ts
+++ app/navigators/create-backup/create-backup-routes.ts
@@ -2,7 +2,7 @@ import { RouteProp } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 export type CreateBackupNavigatorParamList = {
-  Dashboard:
+  CreateBackupDashboard:
     | {
         backupFileName: string;
         backupFilePath: string;
