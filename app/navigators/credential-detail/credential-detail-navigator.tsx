33f3899bf393dcef40c89e7935e07d23fe2a38a5
Developer <info@procivis.ch>
2024-04-23 13:02:54 +0300

    Removed routes, these are part of a follow-up MR

--- app/navigators/credential-detail/credential-detail-navigator.tsx
+++ app/navigators/credential-detail/credential-detail-navigator.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import CredentialDeleteProcessScreen from '../../screens/credential/credential-delete-process-screen';
 import CredentialDetailScreen from '../../screens/credential/credential-detail-screen';
 import { CredentialHistoryScreen } from '../../screens/credential/credential-history-screen';
-import { CredentialDetailNerdView } from '../../screens/credential/credential-nerd-view';
 import { CredentialDetailNavigatorParamList } from './credential-detail-routes';
 
 const Stack = createNativeStackNavigator<CredentialDetailNavigatorParamList>();
@@ -18,10 +17,6 @@ const CredentialDetailNavigator = () => {
         name="DeleteProcessing"
       />
       <Stack.Screen component={CredentialHistoryScreen} name="History" />
-      <Stack.Screen
-        component={CredentialDetailNerdView}
-        name="CredentialNerdView"
-      />
     </Stack.Navigator>
   );
 };
