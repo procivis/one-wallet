56272b074a78ad47a51340c1e30a075dfb410d1d
Developer <info@procivis.ch>
2024-04-25 10:56:13 +0200

    [ONE-1782] Suspended tests with timestamp

--- e2e/page-objects/CredentialDetailScreen.ts
+++ e2e/page-objects/CredentialDetailScreen.ts
@@ -11,6 +11,51 @@ export default abstract class CredentialDetailScreen {
     return element(by.id('CredentialDetailScreen.header.action'));
   }
 
+  private static historyEntry(itemId: number) {
+    const id = `CredentialDetailScreen.history.${itemId}`;
+    return {
+      get did() {
+        return element(by.id(`${id}.did`));
+      },
+      get element() {
+        return element(by.id(id));
+      },
+      get label() {
+        return element(by.id(`${id}.label`));
+      },
+      get time() {
+        return element(by.id(`${id}.timeLabel`));
+      },
+    };
+  }
+
+  static history(itemId: number) {
+    return this.historyEntry(itemId);
+  }
+
+  static historySeeAllButton() {
+    return element(by.id('CredentialDetailScreen.history.seeAll'));
+  }
+
+  private static credentialLabel() {
+    const id = 'CredentialDetailScreen.card.label';
+    return {
+      get element() {
+        return element(by.id(id));
+      },
+      get revokedLabel() {
+        return element(by.id(`${id}.revoked`));
+      },
+      get suspendedLabel() {
+        return element(by.id(`${id}.suspended`));
+      },
+    };
+  }
+
+  static get credential() {
+    return this.credentialLabel();
+  }
+
   private static dataItem(id: string) {
     return {
       get element() {
@@ -32,10 +77,6 @@ export default abstract class CredentialDetailScreen {
 
   static get credentialFormat() {
     return this.dataItem('CredentialDetailScreen.format');
-  }
-
-  static log(itemId: string) {
-    return element(by.id(`CredentialDetailScreen.log.${itemId}`));
   }
 
   static action(actionText: string) {
