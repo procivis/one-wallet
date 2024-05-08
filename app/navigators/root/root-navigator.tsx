fc2968bcc92c4b426472ff42d1fa3c2ec2a266c4
Developer <info@procivis.ch>
2024-05-08 13:19:55 +0200

    Fixed status bar content color

--- app/navigators/root/root-navigator.tsx
+++ app/navigators/root/root-navigator.tsx
@@ -1,12 +1,10 @@
-import { useAppColorScheme } from '@procivis/one-react-native-components';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React, { FunctionComponent } from 'react';
-import { Platform, StatusBar } from 'react-native';
+import { Platform } from 'react-native';
 
 import { useAutomaticPinCodeCoverLogic } from '../../hooks/pin-code/pin-code-check';
 import ImagePreviewScreen from '../../screens/credential/image-preview-screen';
 import PinCodeCheckScreen from '../../screens/onboarding/pin-code-check-screen';
-import { AppColorScheme } from '../../theme';
 import CredentialDetailNavigator from '../credential-detail/credential-detail-navigator';
 import CredentialManagementNavigator from '../credential-management/credential-management-navigator';
 import DashboardNavigator from '../dashboard/dashboard-navigator';
@@ -19,65 +17,56 @@ import { RootNavigatorParamList } from './root-routes';
 const RootStack = createNativeStackNavigator<RootNavigatorParamList>();
 
 const RootNavigator: FunctionComponent = () => {
-  const { darkMode } = useAppColorScheme<AppColorScheme>();
   const initialRouteName = useInitialRoute();
   useAutomaticPinCodeCoverLogic(initialRouteName === 'Dashboard');
 
   return initialRouteName ? (
-    <>
-      <StatusBar
-        animated={true}
-        backgroundColor="transparent"
-        barStyle={darkMode ? 'light-content' : 'dark-content'}
-        translucent={true}
-      />
-      <RootStack.Navigator
-        initialRouteName={initialRouteName}
-        screenOptions={{
-          animationTypeForReplace: 'push',
+    <RootStack.Navigator
+      initialRouteName={initialRouteName}
+      screenOptions={{
+        animationTypeForReplace: 'push',
+        headerShown: false,
+      }}
+    >
+      <RootStack.Group screenOptions={{ gestureEnabled: false }}>
+        <RootStack.Screen
+          component={PinCodeCheckScreen}
+          listeners={{ transitionEnd: () => hideSplashScreen() }}
+          name="PinCodeCheck"
+          options={{
+            animation: 'fade',
+          }}
+        />
+        <RootStack.Screen component={OnboardingNavigator} name="Onboarding" />
+        <RootStack.Screen component={DashboardNavigator} name="Dashboard" />
+      </RootStack.Group>
+      <RootStack.Screen
+        component={NerdModeNavigator}
+        name="NerdMode"
+        options={{
+          animation:
+            Platform.OS === 'android' ? 'slide_from_bottom' : undefined,
           headerShown: false,
+          presentation: 'fullScreenModal',
         }}
-      >
-        <RootStack.Group screenOptions={{ gestureEnabled: false }}>
-          <RootStack.Screen
-            component={PinCodeCheckScreen}
-            listeners={{ transitionEnd: () => hideSplashScreen() }}
-            name="PinCodeCheck"
-            options={{
-              animation: 'fade',
-            }}
-          />
-          <RootStack.Screen component={OnboardingNavigator} name="Onboarding" />
-          <RootStack.Screen component={DashboardNavigator} name="Dashboard" />
-        </RootStack.Group>
-        <RootStack.Screen
-          component={NerdModeNavigator}
-          name="NerdMode"
-          options={{
-            animation:
-              Platform.OS === 'android' ? 'slide_from_bottom' : undefined,
-            headerShown: false,
-            presentation: 'fullScreenModal',
-          }}
-        />
-        <RootStack.Screen
-          component={CredentialManagementNavigator}
-          name="CredentialManagement"
-          options={{
-            animation:
-              Platform.OS === 'android' ? 'slide_from_bottom' : undefined,
-            headerShown: false,
-            presentation: 'formSheet',
-          }}
-        />
-        <RootStack.Screen component={SettingsNavigator} name="Settings" />
-        <RootStack.Screen
-          component={CredentialDetailNavigator}
-          name="CredentialDetail"
-        />
-        <RootStack.Screen component={ImagePreviewScreen} name="ImagePreview" />
-      </RootStack.Navigator>
-    </>
+      />
+      <RootStack.Screen
+        component={CredentialManagementNavigator}
+        name="CredentialManagement"
+        options={{
+          animation:
+            Platform.OS === 'android' ? 'slide_from_bottom' : undefined,
+          headerShown: false,
+          presentation: 'formSheet',
+        }}
+      />
+      <RootStack.Screen component={SettingsNavigator} name="Settings" />
+      <RootStack.Screen
+        component={CredentialDetailNavigator}
+        name="CredentialDetail"
+      />
+      <RootStack.Screen component={ImagePreviewScreen} name="ImagePreview" />
+    </RootStack.Navigator>
   ) : null;
 };
 
