363d60b6ea08174c72f96b5198af3cef676935de
Eugeniu Rusu <rusu@procivis.ch>
2024-05-21 12:49:33 +0200

    ONE-1970: Animation / logic to have icons vertically centered in expanded fields

--- app/components/nerd-view/nerd-mode-item.tsx
+++ app/components/nerd-view/nerd-mode-item.tsx
@@ -5,9 +5,22 @@ import {
   Typography,
   useAppColorScheme,
 } from '@procivis/one-react-native-components';
-import React, { FunctionComponent, useCallback } from 'react';
-import { Linking, StyleSheet, View } from 'react-native';
+import React, {
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+import { Dimensions, Linking, StyleSheet, View, ViewStyle } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import Animated, {
+  clamp,
+  SharedValue,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+} from 'react-native-reanimated';
 
 import { useCopyToClipboard } from '../../hooks/clipboard';
 import { translate } from '../../i18n';
@@ -15,14 +28,21 @@ import { CopyContentIcon, OpenLinkIcon } from '../icon/nerd-view-icon';
 
 const VALUE_PREVIEW_LENGTH = 80;
 
+// The onExpand and expandedAttributes props are used to notify other expanded fields
+// that the layout (and thus the scrollOffset / icon position) has changed. This
+// will trigger a view measurement and repositioning of the icon on all expanded fields.
+
 export type NerdModeItemProps = {
   attributeKey: string;
   attributeText?: string;
   canBeCopied?: boolean;
   element?: React.ReactElement;
+  expandedAttributes?: number;
   highlightedText?: string;
   last?: boolean;
   link?: string;
+  onExpand?: (expanded: boolean) => void;
+  scrollOffset?: SharedValue<number>;
   testID?: string;
 };
 
@@ -30,6 +50,7 @@ const styles = StyleSheet.create({
   actionIcon: {
     alignItems: 'center',
     flex: 0.15,
+    height: 48,
     justifyContent: 'center',
   },
   attributeLabel: {
@@ -60,9 +81,12 @@ const styles = StyleSheet.create({
   },
 });
 
-const ActionIcon: FunctionComponent<{
+const ICON_HEIGHT = 48;
+
+export const ActionIcon: FunctionComponent<{
   copy?: boolean;
   link?: string;
+  style?: ViewStyle;
   testID?: string;
   value: string;
 }> = ({ value, link, testID, copy }) => {
@@ -126,18 +150,93 @@ const NerdModeItem: FunctionComponent<NerdModeItemProps> = ({
   attributeKey,
   link,
   canBeCopied,
+  onExpand,
+  expandedAttributes,
   last = false,
+  scrollOffset,
   element,
   testID,
 }) => {
   const colorScheme = useAppColorScheme();
-  const expandable = attributeText.length > VALUE_PREVIEW_LENGTH;
+  const valueViewRef = React.useRef<View>(null);
 
-  const [expanded, setExpanded] = React.useState(!expandable);
+  const expandedView = useSharedValue({
+    expandedAtOffset: 0,
+    viewHeight: 0,
+    viewStart: 0,
+  });
+
+  const expandable = attributeText.length > VALUE_PREVIEW_LENGTH;
+  const [expanded, setExpanded] = useState(!expandable);
+
+  const screenMiddle = useMemo(() => Dimensions.get('screen').height / 2, []);
+
+  const iconTopPosition = useDerivedValue(() => {
+    if (!expanded || !expandable || !scrollOffset) {
+      return expandedView.value.viewHeight / 2 - ICON_HEIGHT / 2;
+    }
+
+    const viewEnd =
+      expandedView.value.viewStart + expandedView.value.viewHeight;
+    let zeroedOffset = scrollOffset.value - expandedView.value.expandedAtOffset;
+
+    if (expandedView.value.viewStart > screenMiddle) {
+      zeroedOffset =
+        zeroedOffset -
+        (expandedView.value.viewStart - screenMiddle + ICON_HEIGHT / 2);
+    } else {
+      if (viewEnd < screenMiddle + ICON_HEIGHT) {
+        zeroedOffset =
+          zeroedOffset +
+          (screenMiddle - expandedView.value.viewStart - ICON_HEIGHT / 2);
+      } else if (
+        expandedView.value.viewStart < screenMiddle &&
+        viewEnd > screenMiddle
+      ) {
+        zeroedOffset =
+          zeroedOffset +
+          (screenMiddle - expandedView.value.viewStart - ICON_HEIGHT / 2);
+      }
+    }
+
+    return clamp(
+      zeroedOffset,
+      0,
+      expandedView.value.viewHeight - ICON_HEIGHT - 8,
+    );
+  });
+
+  const iconStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: iconTopPosition.value,
+        },
+      ],
+    };
+  });
 
   const previewText = expanded
     ? attributeText
     : attributeText.slice(0, VALUE_PREVIEW_LENGTH) + '...';
+
+  useEffect(() => {
+    if (!scrollOffset) {
+      return;
+    }
+
+    setTimeout(() => {
+      valueViewRef.current?.measure((x, y, w, h, px, py) => {
+        expandedView.value = {
+          expandedAtOffset: scrollOffset.value,
+          viewHeight: h,
+          viewStart: py,
+        };
+      });
+    }, 0);
+    // We only recompute when any attribute is expanded
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [expandedAttributes]);
 
   return (
     <View
@@ -157,6 +256,7 @@ const NerdModeItem: FunctionComponent<NerdModeItemProps> = ({
         {attributeKey}
       </Typography>
       <View
+        ref={valueViewRef}
         style={[
           styles.attributeValueContainer,
           {
@@ -175,19 +275,24 @@ const NerdModeItem: FunctionComponent<NerdModeItemProps> = ({
           />
         )}
         {canBeCopied || link ? (
-          <View style={styles.actionIcon}>
-            <ActionIcon
-              copy={canBeCopied}
-              link={link}
-              testID={concatTestID(testID, 'actionIcon')}
-              value={`${highlightedText}${attributeText}`}
-            />
-          </View>
+          <Animated.View style={[styles.actionIcon, iconStyle]}>
+            {
+              <ActionIcon
+                copy={canBeCopied}
+                link={link}
+                testID={concatTestID(testID, 'actionIcon')}
+                value={`${highlightedText}${attributeText}`}
+              />
+            }
+          </Animated.View>
         ) : null}
       </View>
       {expandable ? (
         <Button
-          onPress={() => setExpanded(!expanded)}
+          onPress={() => {
+            onExpand && onExpand(!expanded);
+            setExpanded(!expanded);
+          }}
           style={styles.expandValueButton}
           testID={concatTestID(testID, 'expandValueButton')}
           title={
