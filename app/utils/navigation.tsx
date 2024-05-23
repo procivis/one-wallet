cbe4452893251f1f625af271ab3266cbd65d30f8
Łukasz Przytuła <lprzytula@gmail.com>
2024-05-23 12:34:00 +0200

    Fixed navigation after onboarding and wallet delete

--- app/utils/navigation.tsx
+++ app/utils/navigation.tsx
@@ -1,9 +1,12 @@
 import {
+  CommonActions,
   NavigationContext,
   NavigationProp,
   NavigationRouteContext,
+  NavigationState,
   Route,
 } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React from 'react';
 
 // Dummy navigation object
@@ -43,3 +46,22 @@ export const OutsideTreeNavigationProvider: React.FC = ({ children }) => (
     </NavigationRouteContext.Provider>
   </NavigationContext.Provider>
 );
+
+type ArrayElement<ArrayType extends readonly unknown[]> =
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+
+export const resetNavigationAction = <
+  ParamList extends Record<string, object | undefined>,
+>(
+  navigator: NativeStackNavigationProp<ParamList, any>,
+  routes: Pick<
+    ArrayElement<NavigationState<ParamList>['routes']>,
+    'name' | 'params'
+  >[],
+) => {
+  const resetAction = CommonActions.reset({
+    index: 0,
+    routes,
+  });
+  navigator.dispatch(resetAction);
+};
