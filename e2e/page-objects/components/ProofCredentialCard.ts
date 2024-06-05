e5d0bfcd5816b66c07e8e994d1d9ee181499657c
Developer <info@procivis.ch>
2024-06-05 15:56:06 +0200

    ONE-1977: e2e Proof request. Selective disclosure. Credential claims

---
+++ e2e/page-objects/components/ProofCredentialCard.ts
@@ -0,0 +1,148 @@
+import { expect } from 'detox';
+
+export const CardSkeleton = (testID: string) => {
+  const cardId = `${testID}.card`;
+  return {
+    get card() {
+      return {
+        get collapsed() {
+          return element(by.id(`${testID}.collapsed`));
+        },
+        get expanded() {
+          return element(by.id(`${testID}.expanded`));
+        },
+      };
+    },
+    claim(index: number) {
+      const claimId = `${testID}.claim.${index}`;
+      return {
+        get element() {
+          return element(by.id(claimId));
+        },
+        get title() {
+          return element(by.id(`${claimId}.title`));
+        },
+        get value() {
+          return element(by.id(`${claimId}.value`));
+        },
+      };
+    },
+    get element() {
+      return element(by.id('CredentialCardSkeleton'));
+    },
+    get header() {
+      const headerId = `${cardId}.header`;
+      return {
+        get element() {
+          return element(by.id(headerId));
+        },
+        get invalid() {
+          return element(by.id(`${headerId}.invalid`));
+        },
+        get missing() {
+          return element(by.id(`${headerId}.missing`));
+        },
+        get multiple() {
+          return element(by.id(`${headerId}.multiple`));
+        },
+      };
+    },
+    get notice() {
+      const noticeId = `${testID}.notice`;
+      return {
+        get element() {
+          return element(by.id(`${cardId}.notice`));
+        },
+        get invalid() {
+          return element(by.id(`${noticeId}.invalid`));
+        },
+        get multiple() {
+          const multipleId = `${noticeId}.multiple`;
+          return {
+            get element() {
+              return element(by.id(multipleId));
+            },
+            get selectButton() {
+              return element(by.id(`${multipleId}.button`));
+            },
+          };
+        },
+        get revoked() {
+          return element(by.id(`${noticeId}.revoked`));
+        },
+        get suspended() {
+          return element(by.id(`${noticeId}.suspended`));
+        },
+        get text() {
+          return element(by.id(`${cardId}.notice.text`));
+        },
+      };
+    },
+  };
+};
+
+export default function ProofCredentialCard(testID: string) {
+  const card = CardSkeleton(testID);
+  return {
+    collapseOrExpand: async function () {
+      await card.header.element.tap();
+    },
+    get element() {
+      return element(by.id(testID));
+    },
+    multipleCredentialsHeaderAvailable: async function () {
+      await expect(card.header.multiple).toBeVisible();
+      await expect(card.header.multiple).toHaveText(
+        'Multiple credentials available',
+      );
+    },
+    openMultipleCredentialsScreen: async function () {
+      await expect(card.notice.multiple.element).toBeVisible();
+      await card.notice.multiple.selectButton.tap();
+    },
+    selectiveDisclosureMessageVisible: async function () {
+      await expect(card.notice.element).toBeVisible();
+      await expect(card.notice.text).toHaveText(
+        'This credential requires you to share all attributes to work.',
+      );
+    },
+    get this() {
+      return card;
+    },
+    verifyClaimValue: async function (
+      index: number,
+      key: string,
+      value: string,
+    ) {
+      await expect(card.claim(index).title).toHaveText(key);
+      await expect(card.claim(index).value).toHaveText(value);
+    },
+    verifyClaimValues: async function (
+      attributes: Array<{ key: string; value: string }>,
+      scrollTo: (
+        element: Detox.IndexableNativeElement,
+        direction: 'up' | 'down',
+      ) => Promise<void>,
+    ) {
+      for (const [index, attribute] of attributes.entries()) {
+        await scrollTo(card.claim(index).element, 'down');
+        await this.verifyClaimValue(index, attribute.key, attribute.value);
+      }
+    },
+
+    verifyIsCardCollapsed: async function (collapsed: boolean = true) {
+      if (collapsed) {
+        await expect(card.card.collapsed).toBeVisible();
+      } else {
+        await expect(card.card.expanded).toBeVisible();
+      }
+    },
+    verifyIsVisible: async function (visible: boolean = true) {
+      if (visible) {
+        await expect(this.element).toBeVisible();
+      } else {
+        await expect(this.element).not.toBeVisible();
+      }
+    },
+  };
+}
--- e2e/page-objects/proof-request/ProofRequestSelectCredentialScreen.ts
+++ e2e/page-objects/proof-request/ProofRequestSelectCredentialScreen.ts
@@ -1,10 +1,22 @@
+import { expect } from 'detox';
+
 export default abstract class ProofRequestSelectCredentialScreen {
   static get screen() {
     return element(by.id('ProofRequestSelectCredentialScreen'));
   }
 
+  static async scrollTo(
+    element: Detox.IndexableNativeElement,
+    direction: 'up' | 'down' = 'down',
+  ) {
+    await waitFor(element)
+      .toBeVisible()
+      .whileElement(by.id('ProofRequestSelectCredentialScreen.scroll'))
+      .scroll(400, direction);
+  }
+
   static get backButton() {
-    return element(by.id('ProofRequestSelectCredentialScreen.header.back'));
+    return element(by.id('Screen.back'));
   }
 
   static get confirmButton() {
@@ -21,13 +33,22 @@ export default abstract class ProofRequestSelectCredentialScreen {
       get notice() {
         const noticeId = `${id}.notice`;
         return {
-          get selectiveDisclosure() {
-            return element(by.id(`${noticeId}.selectiveDisclosure`));
+          get element() {
+            return element(by.id(noticeId));
+          },
+          get text() {
+            return element(by.id(`${noticeId}.text`));
           },
         };
       },
       get selected() {
         return element(by.id(`${id}.selected`));
+      },
+      selectiveDisclosureMessageVisible: async function () {
+        await expect(this.notice.element).toBeVisible();
+        await expect(this.notice.text).toHaveText(
+          'This credential requires you to share all attributes to work.',
+        );
       },
       get unselected() {
         return element(by.id(`${id}.unselected`));
