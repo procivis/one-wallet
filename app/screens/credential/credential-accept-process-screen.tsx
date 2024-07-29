1099f38944102c06dd1c9e55545b445896fd17ce
rusu <rusu@procivis.ch>
2024-07-29 10:47:39 +0300

    Revert the increased submit timeouts

--- app/screens/credential/credential-accept-process-screen.tsx
+++ app/screens/credential/credential-accept-process-screen.tsx
@@ -81,7 +81,7 @@ const CredentialAcceptProcessScreen: FunctionComponent = () => {
         setState(LoaderViewState.Warning);
         setError(e);
       }
-    }, 3000);
+    }, 1000);
   }, [acceptCredential, interactionId, didId]);
 
   useEffect(() => {
