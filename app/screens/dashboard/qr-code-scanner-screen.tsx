952a2d5cc1035e43adbaf9076d7a72145b522087
Pavel Zarecky <zarecky@procivis.ch>
2024-07-30 10:25:59 +0200

    ONE-2666: revert qr code screen permissions

--- app/screens/dashboard/qr-code-scanner-screen.tsx
+++ app/screens/dashboard/qr-code-scanner-screen.tsx
@@ -6,84 +6,12 @@ import React, {
   useEffect,
   useState,
 } from 'react';
-// eslint-disable-next-line react-native/split-platform-components
-import { PermissionsAndroid, Platform, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Code } from 'react-native-vision-camera';
 
 import { useInvitationHandling } from '../../hooks/core/deep-link';
 import { translate } from '../../i18n';
 import { DashboardNavigationProp } from '../../navigators/dashboard/dashboard-routes';
-
-const permissions = [
-  {
-    info: {
-      buttonNegative: 'Cancel',
-      buttonNeutral: 'Ask Me Later',
-      buttonPositive: 'OK',
-      message: 'App needs access to your camera ',
-      title: 'App Camera Permission',
-    },
-    type: PermissionsAndroid.PERMISSIONS.CAMERA,
-  },
-  {
-    info: {
-      buttonNegative: 'Cancel',
-      buttonNeutral: 'Ask Me Later',
-      buttonPositive: 'OK',
-      message: 'App needs access to your bluetooth',
-      title: 'App Bluetooth Permission',
-    },
-    type: PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
-  },
-  {
-    info: {
-      buttonNegative: 'Cancel',
-      buttonNeutral: 'Ask Me Later',
-      buttonPositive: 'OK',
-      message: 'App needs access to your bluetooth',
-      title: 'App Bluetooth Scan Permission',
-    },
-    type: PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
-  },
-  {
-    info: {
-      buttonNegative: 'Cancel',
-      buttonNeutral: 'Ask Me Later',
-      buttonPositive: 'OK',
-      message: 'App needs access to your bluetooth',
-      title: 'App Bluetooth Advertise Permission',
-    },
-    type: PermissionsAndroid.PERMISSIONS.BLUETOOTH_ADVERTISE,
-  },
-  {
-    info: {
-      buttonNegative: 'Cancel',
-      buttonNeutral: 'Ask Me Later',
-      buttonPositive: 'OK',
-      message: 'App needs access to your location',
-      title: 'App Location Permission',
-    },
-    type: PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-  },
-];
-
-const requestCameraPermission = async () => {
-  for await (const permission of permissions) {
-    try {
-      const granted = await PermissionsAndroid.request(
-        permission.type,
-        permission.info,
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log('You can use the camera');
-      } else {
-        console.log('Camera permission denied');
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  }
-};
 
 const QRCodeScannerScreen: FunctionComponent = () => {
   const isFocused = useIsFocused();
@@ -98,12 +26,6 @@ const QRCodeScannerScreen: FunctionComponent = () => {
     },
     [code, setCode],
   );
-
-  useEffect(() => {
-    if (Platform.OS === 'android') {
-      requestCameraPermission();
-    }
-  }, []);
 
   const handleInvitationUrl = useInvitationHandling();
   useEffect(() => {
