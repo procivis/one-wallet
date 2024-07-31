32b48e8889e7ee8ef90021e2d16f03dadb60c6e4
Developer <info@procivis.ch>
2024-07-31 18:05:45 +0200

    * remove unneeded libs

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   assets: ['./assets/fonts/'],
+  dependencies: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    'react-native-camera': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+  },
 };
