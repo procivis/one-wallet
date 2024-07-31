4e594254f0ee2123314c8b8c41a799690aa8e37d
rusu <rusu@procivis.ch>
2024-07-31 18:36:08 +0300

    handle disabled BLE adapter, BLE permissions in invitation flow

---
+++ app/hooks/ble-permissions.ts
@@ -0,0 +1,93 @@
+import { useMemoAsync } from '@procivis/one-react-native-components';
+import { useCallback, useRef, useState } from 'react';
+import { Linking, Platform } from 'react-native';
+import {
+  checkMultiple,
+  openSettings,
+  PERMISSIONS,
+  PermissionStatus,
+  requestMultiple,
+  RESULTS,
+} from 'react-native-permissions';
+
+const permissions =
+  Platform.OS === 'android'
+    ? [
+        PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
+        PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+      ]
+    : [PERMISSIONS.IOS.BLUETOOTH];
+
+export const useBlePermissions = () => {
+  const [interactiveStatus, setInteractiveStatus] =
+    useState<PermissionStatus>();
+
+  const initialStatus = useMemoAsync(async () => {
+    const statuses = await checkMultiple(permissions);
+    return getStrictestResult(Object.values(statuses));
+  }, []);
+
+  const status = interactiveStatus ?? initialStatus;
+
+  const checkPermissions = useCallback(async () => {
+    const statuses = await checkMultiple(permissions);
+    const result = getStrictestResult(Object.values(statuses));
+
+    if (result !== status) {
+      setInteractiveStatus(getStrictestResult(Object.values(statuses)));
+    }
+  }, [status]);
+
+  const requestPermission = useCallback(async () => {
+    if (status !== RESULTS.DENIED) {
+      return;
+    }
+    const results = await requestMultiple(permissions);
+    const summary = getStrictestResult(Object.values(results));
+    if (summary !== status) {
+      setInteractiveStatus(getStrictestResult(Object.values(results)));
+    }
+  }, [status]);
+
+  return { checkPermissions, permissionStatus: status, requestPermission };
+};
+
+const getStrictestResult = (results: PermissionStatus[]) => {
+  return results.reduce((acc, result) => {
+    if (
+      acc === RESULTS.BLOCKED ||
+      acc === RESULTS.DENIED ||
+      acc === RESULTS.UNAVAILABLE
+    ) {
+      return acc;
+    }
+
+    return result;
+  }, RESULTS.GRANTED);
+};
+
+export const useOpenBleSettings = () => {
+  const inFocusFromSettings = useRef<boolean>(false);
+
+  const openBleSettings = useCallback(() => {
+    if (Platform.OS === 'ios') {
+      Linking.openURL('App-Prefs:Bluetooth');
+    } else {
+      Linking.sendIntent('android.settings.BLUETOOTH_SETTINGS');
+    }
+
+    inFocusFromSettings.current = true;
+  }, []);
+
+  const openAppPermissionSettings = useCallback(() => {
+    openSettings();
+    inFocusFromSettings.current = true;
+  }, []);
+
+  return {
+    inFocusFromSettings,
+    openAppPermissionSettings,
+    openBleSettings,
+  };
+};
