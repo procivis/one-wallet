2cbbf86388c20af2e1f3ac376a1ec476a4326066
Developer <info@procivis.ch>
2024-08-27 13:36:55 +0200

    Integrated iso mdl

--- app/hooks/core/proofs.ts
+++ app/hooks/core/proofs.ts
@@ -5,10 +5,13 @@ import {
 } from '@procivis/react-native-one-core';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 
+import { ProcivisExchangeProtocol } from '../../models/proofs';
 import { useONECore } from './core-context';
+import { ONE_CORE_ORGANISATION_ID } from './core-init';
 import { HISTORY_LIST_QUERY_KEY } from './history';
 
 const PROOF_DETAIL_QUERY_KEY = 'proof-detail';
+const PROOF_STATE_QUERY_KEY = 'proof-state';
 
 export const useProofDetail = (proofId: string | undefined) => {
   const { core } = useONECore();
@@ -19,6 +22,22 @@ export const useProofDetail = (proofId: string | undefined) => {
     {
       enabled: Boolean(proofId),
       keepPreviousData: true,
+    },
+  );
+};
+
+export const useProofState = (
+  proofId: string | undefined,
+  isPolling: boolean,
+) => {
+  const { core } = useONECore();
+
+  return useQuery(
+    [PROOF_STATE_QUERY_KEY, proofId],
+    () => core.getProof(proofId!).then((proof) => proof.state),
+    {
+      enabled: Boolean(proofId),
+      refetchInterval: isPolling ? 1000 : false,
     },
   );
 };
@@ -74,9 +93,11 @@ export const useProofReject = () => {
 
 export const useProposeProof = () => {
   const queryClient = useQueryClient();
+  const { core } = useONECore();
 
   return useMutation(
-    async () => ({ shareUrl: 'this is a very fake qr code' }),
+    async (exchange: ProcivisExchangeProtocol) =>
+      core.proposeProof(exchange, ONE_CORE_ORGANISATION_ID),
     {
       onSuccess: () => {
         queryClient.invalidateQueries(HISTORY_LIST_QUERY_KEY);
---
+++ app/models/proofs.ts
@@ -0,0 +1,5 @@
+export enum ProcivisExchangeProtocol {
+  ISO_MDL = 'ISO_MDL',
+  OPENID4VC = 'OPENID4VC',
+  PROCIVIS = 'PROCIVIS_TEMPORARY',
+}
