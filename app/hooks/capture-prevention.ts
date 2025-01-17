aad598c47187eea9428df52ecb3ec5f46b69ea69
Developer <info@procivis.ch>
2025-01-17 15:17:03 +0100

    ONE-3822: screen capture prevention

---
+++ app/hooks/capture-prevention.ts
@@ -0,0 +1,42 @@
+import { useIsFocused } from '@react-navigation/native';
+import { useEffect } from 'react';
+import { Platform } from 'react-native';
+import RNScreenshotPrevent from 'react-native-screenshot-prevent';
+
+let enabledCounter = 0;
+const enable = () => {
+  if (!enabledCounter++) {
+    if (Platform.OS === 'android') {
+      RNScreenshotPrevent.enabled(true);
+    } else {
+      RNScreenshotPrevent.enableSecureView();
+    }
+  }
+};
+const disable = () => {
+  if (!--enabledCounter) {
+    if (Platform.OS === 'android') {
+      RNScreenshotPrevent.enabled(false);
+    } else {
+      RNScreenshotPrevent.disableSecureView();
+    }
+  }
+};
+
+/**
+ * Prevents screenshot and video capture of the current screen
+ * @param enabled Flag for temporary disable
+ */
+export function useCapturePrevention(enabled = true) {
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (enabled && isFocused) {
+      enable();
+
+      return () => {
+        disable();
+      };
+    }
+  }, [enabled, isFocused]);
+}
