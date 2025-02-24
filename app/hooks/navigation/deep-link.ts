bfc7f209e2ed64218929844805b54a25a626665a
Developer <info@procivis.ch>
2025-02-24 10:52:15 +0100

    Fixed handling of deeplinks when app is running

--- app/hooks/navigation/deep-link.ts
+++ app/hooks/navigation/deep-link.ts
@@ -4,17 +4,50 @@ import {
   useRuntimeDeepLinkHandling as useCommonRuntimeDeepLinkHandling,
 } from '@procivis/one-react-native-components';
 import { useNavigation } from '@react-navigation/native';
-import { useCallback } from 'react';
+import { useCallback, useRef } from 'react';
 
 import { RootNavigationProp } from '../../navigators/root/root-routes';
+import {
+  getCurrentRoute,
+  RootNavigationStateUpdatedEvent,
+} from '../../utils/navigation';
 import { usePinCodeInitialized } from '../pin-code/pin-code';
 
 export const useRuntimeDeepLinkHandling = () => {
+  const navigation = useNavigation<RootNavigationProp>();
+  const stateListener = useRef<() => void>();
   const handleInvitationUrl = useInvitationHandling();
   const pinInitialized = usePinCodeInitialized();
+
+  const handleRuntimeInvitationLink = useCallback(
+    (url: string) => {
+      const currentRoute = getCurrentRoute(navigation.getState?.() || {});
+
+      if (currentRoute !== 'PinCodeCheck') {
+        handleInvitationUrl(url);
+        return;
+      }
+
+      stateListener.current?.();
+      stateListener.current = navigation.addListener(
+        'state',
+        (e: RootNavigationStateUpdatedEvent) => {
+          const currentStateRoute = getCurrentRoute(e.data.state);
+          if (currentStateRoute === 'PinCodeCheck') {
+            return;
+          }
+          handleInvitationUrl(url);
+          stateListener.current?.();
+          stateListener.current = undefined;
+        },
+      );
+    },
+    [handleInvitationUrl, navigation],
+  );
+
   useCommonRuntimeDeepLinkHandling(
     Boolean(pinInitialized),
-    handleInvitationUrl,
+    handleRuntimeInvitationLink,
   );
 };
 
