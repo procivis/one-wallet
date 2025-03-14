d3c5edd24be4108903267e566b3bd91a92a67dd4
Developer <info@procivis.ch>
2025-03-14 15:37:50 +0000

    Update One Core bindings to v1.37097.0

--- app/navigators/app-navigator.tsx
+++ app/navigators/app-navigator.tsx
@@ -47,6 +47,22 @@ const coreConfig = {
     ISO_MDL: {
       disabled: !config.featureFlags.isoMdl,
     },
+    MDOC_OPENID4VP: {
+      params: {
+        private: {
+          encryption:
+            '93d9182795f0d1bec61329fc2d18c4b4c1b7e65e69e20ec30a2101a9875fff7e',
+        },
+      },
+    },
+    OPENID4VC: {
+      params: {
+        private: {
+          encryption:
+            '93d9182795f0d1bec61329fc2d18c4b4c1b7e65e69e20ec30a2101a9875fff7e',
+        },
+      },
+    },
   },
   keyStorage: {
     INTERNAL: {
