f54bdf37519d64d47097e1c9436b76c7c078f871
Developer <info@procivis.ch>
2025-07-21 14:21:05 +0200

    feat: rename credential-schemas -> ecosystem-assets

--- app/utils/credential-schemas.ts
+++ app/utils/credential-schemas.ts
@@ -2,7 +2,7 @@ import { ImportCredentialSchemaRequestSchema } from '@procivis/react-native-one-
 
 export const getCredentialSchemasFromAssets = () => {
   const schemas = require.context(
-    '../../assets/credential-schemas',
+    '../../assets/ecosystem-assets',
     true,
     /\.json$/,
   );
