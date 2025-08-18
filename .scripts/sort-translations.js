79fdf39e09be8b3891d03697df9c39ea18b9b2e5
Developer <info@procivis.ch>
2025-08-18 09:54:22 +0200

    Fix translations

--- .scripts/sort-translations.js
+++ .scripts/sort-translations.js
@@ -1,4 +1,5 @@
 const shell = require('shelljs');
+const fs = require('fs');
 const translationDirs = [
   `${__dirname}/../app/i18n/de`,
   `${__dirname}/../app/i18n/en`,
@@ -6,24 +7,11 @@ const translationDirs = [
 const translationsToSort = shell.find(translationDirs).filter(function (file) {
   return file.endsWith('.json');
 });
-
-// also check that translations are nested fully, with no (partially) inlined keys
-const checkKeysDontContainSeparators = (obj, path, filename) => {
-  if (typeof obj === 'object') {
-    for (const key of Object.keys(obj)) {
-      const fullPath = path ? `${path}.${key}` : key;
-      if (key.includes('.')) {
-        throw new Error(`Invalid key in ${filename} at path: ${fullPath}`);
-      }
-      checkKeysDontContainSeparators(obj[key], fullPath, filename);
-    }
-  }
-};
 for (const translationFile of translationsToSort) {
-  checkKeysDontContainSeparators(require(translationFile), '', translationFile);
-}
-
-for (const translationFile of translationsToSort) {
+  shell.exec(
+    `${__dirname}/../node_modules/.bin/flat ${translationFile} > ${translationFile}.flat`,
+  );
+  fs.renameSync(`${translationFile}.flat`, translationFile);
   shell.exec(
     `${__dirname}/../node_modules/.bin/sort-json --indent-size=2 ${translationFile}`,
   );
