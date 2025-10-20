5661926735cf2ecd0462f4f9ed1b7c80ab5a44b6
Developer <info@procivis.ch>
2025-10-20 10:03:32 +0200

    Fixed parsing of PresentationDefinitionV2 credential sets for grouping simple sets

--- app/utils/credential-sharing-v2.tsx
+++ app/utils/credential-sharing-v2.tsx
@@ -1,4 +1,8 @@
-import { ShareCredentialGroupLabels } from '@procivis/one-react-native-components';
+import {
+  ArrayElement,
+  ShareCredentialGroupLabels,
+} from '@procivis/one-react-native-components';
+import { PresentationDefinitionV2 } from '@procivis/react-native-one-core';
 
 import { translate } from '../i18n';
 import { shareCredentialLabels } from './credential-sharing';
@@ -8,4 +12,59 @@ export const shareCredentialGroupLabels = (): ShareCredentialGroupLabels => {
     ...shareCredentialLabels(),
     groupHeader: translate('info.proofRequest.group.header'),
   };
+};
+
+export type CredentialSet = ArrayElement<
+  PresentationDefinitionV2['credentialSets']
+> & {
+  id: string;
+  valid: boolean;
+};
+
+export const credentialSetsFromPresentationDefinitionV2 = (
+  presentationDefinition: PresentationDefinitionV2,
+): CredentialSet[] => {
+  return presentationDefinition.credentialSets.map((set, index) => {
+    const filteredOptions = set.options.filter((group) => {
+      const arrayGroup = Array.isArray(group) ? group : [group];
+      return arrayGroup.every((queryId) => {
+        const query = presentationDefinition.credentialQueries[queryId];
+        return queryId && 'applicableCredentials' in query;
+      });
+    });
+    return {
+      ...set,
+      id: index.toString(),
+      options: filteredOptions.length ? filteredOptions : set.options,
+      valid: Boolean(filteredOptions.length),
+    };
+  });
+};
+
+type SortedCredentialSets = {
+  optionSets: CredentialSet[];
+  simpleSets: CredentialSet[];
+};
+
+export const sortedCredentialSets = (
+  credentialSets: CredentialSet[],
+): SortedCredentialSets => {
+  const simpleSets: CredentialSet[] = [];
+  credentialSets.forEach((set) => {
+    if (!set.required || set.options.length > 1) {
+      return;
+    }
+    const options = set.options.filter(
+      (option) => !simpleSets.some((s) => s.options.includes(option)),
+    );
+    simpleSets.push({
+      ...set,
+      options,
+    });
+  });
+  const optionSets = credentialSets.filter(
+    (set) => !simpleSets.some((s) => s.id === set.id),
+  );
+
+  return { optionSets, simpleSets };
 };
