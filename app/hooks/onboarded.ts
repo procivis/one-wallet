7152b4bbdb7eb23a90d459c1d2001082ccd3651d
Developer <info@procivis.ch>
2026-06-04 16:18:48 +0200

    [ONE-10139] Fixed broken deep links right after onboarding

--- app/hooks/onboarded.ts
+++ app/hooks/onboarded.ts
@@ -1,9 +1,16 @@
 import { useONECore } from '@procivis/one-react-native-components';
 import { useEffect, useState } from 'react';
 
+import { useOnPinCodeInitialized } from './pin-code/pin-code';
+
 export const useIsOnboarded = () => {
   const { core, organisationId } = useONECore();
   const [onboarded, setOnboarded] = useState<boolean | undefined>(undefined);
+
+  useOnPinCodeInitialized(() => {
+    setOnboarded(undefined);
+  });
+
   useEffect(() => {
     if (onboarded !== undefined) {
       return;
